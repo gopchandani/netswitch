99c1af04bc42e56635970116c89cbb244d4947aa
rakesh <gopchandani@gmail.com>
2013-12-16 20:08:48 -0600

    Throwing parameters in the mix.

--- 541simulationproject/src/Driver.py
+++ 541simulationproject/src/Driver.py
@@ -13,20 +13,29 @@ from Update import Update
 class Driver(object):
     def __init__ (self, num_iterations):
         
+        self.param1 = numpy.arange(1.0, 10.0, 1.0)
+        self.param2 = numpy.arange(1.0, 10.0, 1.0)
+        
         self.num_iterations = num_iterations
         self.env = None
-        self.avg_wait_times = []
+        
+        self.avg_wait_times = {}
 
-
-    def run_simulation(self):
+    def run_iterations(self, param1, param2):
+        self.avg_wait_times[(param1, param2)]  = []
         
         for i in range(self.num_iterations):
             random.seed()
             self.env = simpy.Environment()
-            self.update = Update(self.env, 1.0, 0.1, 100)
+            self.update = Update(self.env, param1, param2, 100)
             self.env.run(until=10000)
-            self.avg_wait_times.append(numpy.average(self.update.update_wait_times))
-            
+            self.avg_wait_times[(param1, param2)].append(numpy.average(self.update.update_wait_times))
+                        
+    def run_simulation(self):
+        for param1 in self.param1:
+            for param2 in self.param2:
+                self.run_iterations(param1, param2)
+                                        
     def process_output(self):
         print self.avg_wait_times
 
